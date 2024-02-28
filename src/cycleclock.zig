b08c06212dfe3be9b6c12366efe59b80b409d536
Will Manning <will@willmanning.io>
2024-02-28 12:37:20 -0500

    fix typo and improve test

--- src/cycleclock.zig
+++ src/cycleclock.zig
@@ -11,13 +11,13 @@ pub inline fn now() u64 {
     }
 
     if (comptime cpu.arch == .x86_64) {
-        return now_x64_64();
+        return now_x86_64();
     }
 
     @compileError("Unsupported CPU architecture: " ++ @tagName(cpu.arch));
 }
 
-inline fn now_x64_64() u64 {
+inline fn now_x86_64() u64 {
     var low: u64 = undefined;
     var high: u64 = undefined;
     asm volatile ("rdtsc"
@@ -28,8 +28,11 @@ inline fn now_x64_64() u64 {
 }
 
 test "simple test" {
-    std.debug.print("Cycle Time: {}!\n", .{now()});
-    std.debug.print("Cycle Time: {}!\n", .{now()});
+    const first = now();
+    std.debug.print("Cycle Time: {}!\n", .{first});
+    const second = now();
+    std.debug.print("Cycle Time: {}!\n", .{second});
+    try std.testing.expect(first < second);
 }
 
 comptime {
