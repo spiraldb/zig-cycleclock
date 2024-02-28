8890bc4a5c8229a646020499eed2b5ddbaa638f5
Nicholas Gates <gatesn@users.noreply.github.com>
2024-02-28 15:01:19 +0000

    x86 (#2)

--- src/cycleclock.zig
+++ src/cycleclock.zig
@@ -10,7 +10,21 @@ pub inline fn now() u64 {
         return kperf.get_counter() catch 0;
     }
 
+    if (comptime cpu.arch == .x86_64) {
+        return now_x64_64();
+    }
+
     @compileError("Unsupported CPU architecture: " ++ @tagName(cpu.arch));
+}
+
+inline fn now_x64_64() u64 {
+    var low: u64 = undefined;
+    var high: u64 = undefined;
+    asm volatile ("rdtsc"
+        : [low] "=r" (low),
+          [high] "=r" (high),
+    );
+    return (high << 32) | low;
 }
 
 test "simple test" {
