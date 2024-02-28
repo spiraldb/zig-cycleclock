784f469bde89dd526901e0f99dac732f0c7c96a2
Nicholas Gates <nick@nickgates.com>
2024-02-28 17:24:55 +0000

    Fix x86 clock

--- src/cycleclock.zig
+++ src/cycleclock.zig
@@ -13,9 +13,22 @@ pub inline fn now() u64 {
     @compileError("Unsupported CPU architecture: " ++ @tagName(cpu.arch));
 }
 
+<<<<<<< Updated upstream
+=======
+inline fn now_x64_64() u64 {
+    var low: u64 = undefined;
+    var high: u64 = undefined;
+    asm volatile ("rdtsc"
+        : [low] "={eax}" (low),
+          [high] "={edx}" (high),
+    );
+    return (high << 32) | low;
+}
+
+>>>>>>> Stashed changes
 test "simple test" {
-    const time = now();
-    std.debug.print("Cycle Time: {}!\n", .{time});
+    std.debug.print("Cycle Time: {}!\n", .{now()});
+    std.debug.print("Cycle Time: {}!\n", .{now()});
 }
 
 comptime {
