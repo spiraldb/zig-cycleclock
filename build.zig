2a0d2e7a361dd171c6529ad7ff5b9a12487c7b88
dweiller <4678790+dweiller@users.noreply.github.com>
2024-07-11 16:44:51 +1000

    use std.Build.path()

--- build.zig
+++ build.zig
@@ -5,16 +5,16 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("cycleclock", .{
-        .root_source_file = .{ .path = "src/cycleclock.zig" },
+        .root_source_file = b.path("src/cycleclock.zig"),
     });
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/cycleclock.zig" },
+        .root_source_file = b.path("src/cycleclock.zig"),
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    lib_unit_tests.addIncludePath(.{ .path = "src" });
+    lib_unit_tests.addIncludePath(b.path("src"));
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
